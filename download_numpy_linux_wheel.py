3f0cdcf9caf5529623a9425d31d1d37edb697127
KyungChan-PARK <packr0723@gmail.com>
2025-05-28 20:57:04 +0900

    e
    e

---
+++ download_numpy_linux_wheel.py
@@ -0,0 +1,10 @@
+import urllib.request
+import os
+
+url = "https://github.com/numpy/numpy/releases/download/v2.2.6/numpy-2.2.6-cp313-cp313-manylinux_2_17_x86_64.whl"
+out = os.path.join("deps", "numpy-2.2.6-cp313-cp313-manylinux_2_17_x86_64.whl")
+
+os.makedirs("deps", exist_ok=True)
+print(f"Downloading: {url}\n  to: {out}")
+urllib.request.urlretrieve(url, out)
+print("다운로드 완료:", out) 
